f36d45d1baa46eb6d7d0891c7cc5c9746f101f3f
CryoNero <51859381+CryoNero@users.noreply.github.com>
2019-07-16 04:06:53 -0400

    Ubuntu18.04

--- ubuntu.sh
+++ ubuntu.sh
@@ -22,7 +22,7 @@ if [ "$first" != 1 ];then
 		*)
 			echo "unknown architecture"; exit 1 ;;
 		esac
-		wget "https://partner-images.canonical.com/core/disco/current/ubuntu-disco-core-cloudimg-${archurl}-root.tar.gz" -O $tarball
+		wget "https://partner-images.canonical.com/core/bionic/20190708/ubuntu-bionic-core-cloudimg-armhf-root.tar.gz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
